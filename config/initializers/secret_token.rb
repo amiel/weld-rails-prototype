53d5c49679055275bfeb639cc87f0e2e1d17e442
Amiel Martin <amiel.martin@gmail.com>
2011-09-10 16:10:46 -0700

    New rails app with therubyracer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeldApp::Application.config.secret_token = '2a51a6a7972a5181e699b815588d86d45e5b11cce1e4daf922c9242a223a300f3097b561f84a726a63d541dad2d49b7b7ab178ba464f693731426f8d18bebe20'
